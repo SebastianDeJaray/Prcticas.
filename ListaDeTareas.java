97ef1a45b1ec72dd8e651f201f16396e20ab79fc
SebastianDeJaray <143725068+SebastianDeJaray@users.noreply.github.com>
2023-09-05 08:46:37 -0600

    Add files via upload

---
+++ ListaDeTareas.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package variables;
+
+/**
+ *
+ * @author labcca
+ */
+public class ListaDeTareas {
+    public  static void main(String[] args){
+            //Todo code application logic here
+            int edad;
+            String Tarea1 = "Lavar el carro";
+            String Tarea2 = " hacer un proyecto";
+            String Tarea3 = " cocinar";
+            String Tarea4 = "trapear";
+            String Tarea5 = "hacer ejercicio";
+     
+            
+            System.out.println("Primera tarea: " + Tarea1 + " Segunda tarea:" + Tarea2 + " Tercer tarea:"  + Tarea3 + " Cuarta tarea: " + Tarea4 + " Quinta tarea: " + Tarea5);
+    }        
+}

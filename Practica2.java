646b94bde88f8c0b2e7ca7b8a75b572bee9d55a1
SebastianDeJaray <143725068+SebastianDeJaray@users.noreply.github.com>
2023-09-07 08:53:38 -0600

    Add files via upload

---
+++ Practica2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author labcca
+ */
+public class Practica2 {
+     public static void main(String[] args) {
+       
+       int altura = 7;
+       int base = 6;
+       
+       String nombre = "Sebastian Brando";
+       int arearectangulo = base*altura;
+    
+         System.out.println("La altura del rectangulo es: " + arearectangulo);
+         System.out.println("Mi nombre es: " + nombre);
+       
+       
+       
+     } 
+}
